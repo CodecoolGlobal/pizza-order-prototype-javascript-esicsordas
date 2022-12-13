98ae8749a430efd5b63a202dacb20d9780e2c994
tomatams <tomatams@gmail.com>
2022-12-13 13:10:04 +0100

    started filter for allergens

--- frontend/index.js
+++ frontend/index.js
@@ -8,6 +8,10 @@ const potionComponent = ({name, ingredients, period, price, allergens}) => `
 	</div>
 `;
 
+const clickEvent = event => {
+	console.log(event.target);
+}
+
 function getData (link, action){
     let fullUrl = `http://localhost:3000/api/${link}`;
     fetch(fullUrl)
@@ -25,15 +29,42 @@ function displayData (data){
 }
 
 function makeAllergenList (data){
-	console.log("allergének", data);
 	let rootElement = document.getElementById("root");
-	let createLIelement = document.createElement("li");
+	let ul = document.createElement("ul");
+	ul.id = "allergens";
+		//allergének listába rakása
+	for (let i = 0; i < data.length; i++){
+		let newLI = document.createElement("li");
+		newLI.innerText = data[i].name;
+		ul.appendChild(newLI);
+	}
+	rootElement.appendChild(ul);
+
+	// Create a "close" button and append it to each list item
+	let myNodelist = document.getElementsByTagName("LI");
+	for (let i = 0; i < myNodelist.length; i++) {
+		let span = document.createElement("SPAN");
+		let txt = document.createTextNode("\u00D7");
+		span.className = "close";
+		span.appendChild(txt);
+		myNodelist[i].appendChild(span);
+	}
+
+	// Click on a close button to hide the current list item
+	let  close = document.getElementsByClassName("close");
+	for (let i = 0; i < close.length; i++) {
+		close[i].onclick = function() {
+			var div = this.parentElement;
+			div.style.display = "none";
+		}
+	}
 	
 }
 
 function main(){
 	getData("potions", displayData);
-	//getData("allergens", makeAllergenList);
+	getData("allergens", makeAllergenList);
+	window.addEventListener("click", clickEvent)
 }
 
 main ();
